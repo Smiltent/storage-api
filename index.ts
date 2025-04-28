b224b1fccfdf2fc790a8d31c5194e55454ed5637
Smiltent <me@smilt.dev>
2025-04-28 11:27:49 +0300

    data thing!

--- index.ts
+++ index.ts
@@ -30,7 +30,7 @@ app.use('/files', express.static(path.join(__dirname, 'files'), {
 app.post('/v1/upload', (req, res) => {
     if (req.get('X-API-Key') !== process.env.API_KEY) {
         res.status(400).send({ res: "Nuh uh..." });
-        return
+        return;
     }
 
     if (req.files && req.files.file) {
@@ -39,22 +39,46 @@ app.post('/v1/upload', (req, res) => {
         const extension = path.extname(file?.name as string);
         const uuid = uuidv4();
 
-        file?.mv(path.join(__dirname, 'files', `${uuid}${extension}`), (err) => {
+        file?.mv(path.join(__dirname, 'files', `${uuid}.${extension}`), (err) => {
             if (err) {
-                res.status(400).send({ res: `Failed to save file` })
-                return
+                res.status(400).send({ res: `Failed to save file` });
+                return;
             }
-            res.status(200).send({ res: `${process.env.URL}/files/${uuid}${extension}` })
+
+            const dataFilePath = path.join(__dirname, 'data.json');
+            let data: { filetypes: { [key: string]: number }, amountOfFiles: number } = {
+                filetypes: {},
+                amountOfFiles: 0
+            };
+
+            if (fs.existsSync(dataFilePath)) {
+                const rawData = fs.readFileSync(dataFilePath, 'utf-8');
+                data = JSON.parse(rawData);
+            }
+
+            data.filetypes[extension] = (data.filetypes[extension] || 0) + 1;
+            data.amountOfFiles += 1;
+
+            fs.writeFileSync(dataFilePath, JSON.stringify(data, null, 4));
+            res.status(200).send({ res: `${process.env.URL}/files/${uuid}.${extension}` });
         });
 
     } else {
         res.status(400).send({ res: "No files uploaded" });
     }
-})
+});
 
 app.get('/v1/data', (req, res) => {
-    res.status(200).send()
-})
+    const dataFilePath = path.join(__dirname, 'data.json');
+
+    if (fs.existsSync(dataFilePath)) {
+        const rawData = fs.readFileSync(dataFilePath, 'utf-8');
+        const data = JSON.parse(rawData);
+        res.status(200).send(data);
+    } else {
+        res.status(404).send({ res: "Data file not found" });
+    }
+});
 
 app.get('/', (_req, res) => {
     res.redirect("https://github.com/Smiltent/storage-api")
